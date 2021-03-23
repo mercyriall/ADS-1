2295b7a2ffed4d0be4364940eaf872e377295d61
mercyriall <73515384+mercyriall@users.noreply.github.com>
2021-03-23 17:35:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int R, L, sum, midl;
+    sum = 0;
+    R = size - 1;
+    while(L < R){
+        midl = (R-L)/2;
+        if(arr[midl] == value){
+            while(arr[midl - 1] == value){
+                sum++;
+                midl--;
+            }
+            midl += sum;
+            while(arr[midl] == value){
+                midl++;
+                sum++;
+            }
+        }
+        if(arr[midl] > value){
+            R = midl;
+        }
+        else{
+            L = midl;
+        }
+    }
+  return sum; // если ничего не найдено
 }
