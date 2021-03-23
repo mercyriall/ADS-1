06bf2fde7da6dc0102092d54691a547b6661f3c7
mercyriall <73515384+mercyriall@users.noreply.github.com>
2021-03-23 18:17:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int cbinsearch(int *arr, int size, int value) {
     int R, L = 0, sum = 0, midl;
     R = size - 1;
     while(L < R){
-        midl = (R-L)/2;
+        midl = (R - L) / 2;
         if(arr[midl] == value){
             while(arr[midl - 1] == value){
                 sum++;
@@ -20,7 +20,7 @@ int cbinsearch(int *arr, int size, int value) {
             R = midl;
         }
         else{
-            L = midl +1;
+            L = midl + 1;
         }
     }
     return 0; // если ничего не найдено
